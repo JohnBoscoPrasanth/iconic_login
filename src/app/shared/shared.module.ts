2465836d611a8cd503b2da95d4314e8dcd38bd04
John Bosco Prasanth <johnbosco.george@twilightsoftwares.com>
2024-07-09 20:29:54 +0530

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+import { ButtonComponent } from '../components/button/button.component';
+import { ContentComponent } from '../components/content/content.component';
+import { HeaderComponent } from '../components/header/header.component';
+
+@NgModule({
+  declarations: [ButtonComponent, ContentComponent, HeaderComponent],
+  imports: [CommonModule, IonicModule],
+  exports: [ButtonComponent, ContentComponent, HeaderComponent],
+})
+export class SharedModule {}
